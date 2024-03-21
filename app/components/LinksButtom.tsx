3ed86399fc471e590370905367ce607e772f658c
Vizeu <leovizeu@hotmail.com>
2024-03-20 23:07:00 -0300

    starting to work

--- app/components/LinksButtom.tsx
+++ app/components/LinksButtom.tsx
@@ -1,15 +1,18 @@
 import Link from "next/link"
 
 const LinksButtom = ({ href, title }: { href: string, title: string }) => {
-  <button className="bg-blue-300">
-    <Link
-      href={href}
-      className="text-base font-medium leading-6 text-primary-500 hover:text-primary-600 dark:hover:text-primary-400"
-      aria-label={`Link to ${title}`}
-    >
-      Learn more &rarr;
-    </Link>
-  </button>
+  return (
+    <button className="bg-blue-500 px-52 py-3 rounded-full hover:bg-white hover:text-black shadow-lg shadow-sky-600 hover:shadow-white duration-200">
+      <Link
+        href={href}
+        className="text-base font-medium leading-6 text-primary-500 hover:text-primary-600 dark:hover:text-primary-400"
+      >
+        <h1 className="font-semibold">
+          {title}
+        </h1>
+      </Link>
+    </button>
+  )
 }
 
 export default LinksButtom
--- app/page.tsx
+++ app/page.tsx
@@ -2,25 +2,12 @@ import LinksButtom from '@/app/components/LinksButtom'
 
 export default function Home() {
   return (
-    <main>
+    <main className='min-h-screen flex items-center justify-center'>
       <div>
         
       </div>
       <div>
-        <ul>
-          <li>
-            <LinksButtom href='https://github.com/leovizeu' title='Github'> Github </LinksButtom>
-          </li>
-          <li>
-            <h1>LinkedIn</h1>
-          </li>
-          <li>
-            <h1>Twitter</h1>
-          </li>
-          <li>
-            <h1>Instagram</h1>
-          </li>
-        </ul>
+        <LinksButtom href={'https://github.com/leovizeu'} title={'Github'} />
       </div>
     </main>
   );
