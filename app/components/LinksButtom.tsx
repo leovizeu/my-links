4635893f816c3a6b0a0a78f6524e93b8f83ce114
Vizeu <leovizeu@hotmail.com>
2024-03-20 23:22:29 -0300

    not going the way I planned

--- app/components/LinksButtom.tsx
+++ app/components/LinksButtom.tsx
@@ -2,16 +2,21 @@ import Link from "next/link"
 
 const LinksButtom = ({ href, title }: { href: string, title: string }) => {
   return (
-    <button className="bg-blue-500 px-52 py-3 rounded-full hover:bg-white hover:text-black shadow-lg shadow-sky-600 hover:shadow-white duration-200">
-      <Link
-        href={href}
-        className="text-base font-medium leading-6 text-primary-500 hover:text-primary-600 dark:hover:text-primary-400"
-      >
-        <h1 className="font-semibold">
-          {title}
-        </h1>
-      </Link>
-    </button>
+    <div className="">
+      <button className="bg-white text-black rounded-full outline outline-white hover:bg-transparent hover:text-white duration-200 px-52 py-3">
+        <Link
+          href={href}
+        >
+          <div className="h-full w-full flex">
+            <h1 className="font-semibold">
+              {title}
+              
+            </h1>
+          <a href="" className="flex justify-end">...</a>
+          </div>
+        </Link>
+      </button>
+    </div>
   )
 }
 
